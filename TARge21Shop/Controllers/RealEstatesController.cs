db3904a1ca95e0a38a1dc80e5f4d2454b3df9470
AHammer15 <100429585+AHammer15@users.noreply.github.com>
2023-02-02 11:34:03 +0200

    RealEstateDetails
    A

--- TARge21Shop/Controllers/RealEstatesController.cs
+++ TARge21Shop/Controllers/RealEstatesController.cs
@@ -86,6 +86,48 @@ namespace TARge21Shop.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var realEstate = await _realEstatesServices.GetAsync(id);
+
+            if (realEstate == null)
+            {
+                return NotFound();
+            }
+
+            //var photos = await _context.FileToDatabase
+            //    .Where(x => x.SpaceshipId == id)
+            //    .Select(y => new ImageViewModel
+            //    {
+            //        SpaceshipId = y.Id,
+            //        ImageId = y.Id,
+            //        ImageData = y.ImageData,
+            //        ImageTitle = y.ImageTitle,
+            //        Image = string.Format("data:image/gif;base64, {0}", Convert.ToBase64String(y.ImageData))
+            //    }).ToArrayAsync();
+
+            var vm = new RealEstateCreateUpdateViewModel();
+
+            vm.Id = realEstate.Id;
+            vm.Address = realEstate.Address;
+            vm.City = realEstate.City;
+            vm.Country = realEstate.Country;
+            vm.Size = realEstate.Size;
+            vm.Price = realEstate.Price;
+            vm.Floor = realEstate.Floor;
+            vm.Region = realEstate.Region;
+            vm.Phone = realEstate.Phone;
+            vm.Fax = realEstate.Fax;
+            vm.PostalCode = realEstate.PostalCode;
+            vm.RoomCount = realEstate.RoomCount;
+            vm.CreatedAt = realEstate.CreatedAt;
+            vm.ModifiedAt = realEstate.ModifiedAt;
+            //vm.Image.AddRange(photos);
+
+            return View("CreateUpdate", vm);
+        }
+
+        [HttpGet]
         public async Task<IActionResult> Delete(Guid id)
         {
             var realEstate = await _realEstatesServices.GetAsync(id);
